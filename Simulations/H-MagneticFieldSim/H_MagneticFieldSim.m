451d87d7b18e565c7974eee20df3bce8f21a164d
OzMelzer <oz.melzer@gmail.com>
2025-10-27 19:07:28 -0600

    made comments multiline

--- Simulations/H-MagneticFieldSim/H_MagneticFieldSim.m
+++ Simulations/H-MagneticFieldSim/H_MagneticFieldSim.m
@@ -21,15 +21,19 @@ a = Re + alt;
 i_deg = 55.0; 
     %initial argument of latitude in degrees
 u0_deg = 0.0;
-    %Gravitational effect of earth (mu = GM) (Km^3/s^2) G is newtons constant of
-    %gravitation and m is mass of earth. (source - pg 198, Space Mission
-    %Engineering, Wertz, Everett, and puschell)
+    %{
+    Gravitational effect of earth (mu = GM) (Km^3/s^2) G is newtons constant of
+    gravitation and m is mass of earth. (source - pg 198, Space Mission
+    Engineering, Wertz, Everett, and puschell)
+    %}
 MuEarth = 398600.4418;
 
-    %Calculate Heq Magnetic field strength at earths equator
-        %Earths horizontal field intensity in Teslas (T) based on NOAA data
-        %(8/1/2026),(LAT 0, LONG 0, ALT 400km)(HorizontalIntensity bc mag
-        %field is horizontal at equator
+%{
+Calculate Heq Magnetic field strength at earths equator
+    Earths horizontal field intensity in Teslas (T) based on NOAA data
+    (8/1/2026),(LAT 0, LONG 0, ALT 400km)(HorizontalIntensity bc mag
+    field is horizontal at equator
+%}
     Beq = 0.0000226068;
             %calculate Magnetic Flux density based on relationship to magnetic
             %field strength [ Heq = Beq / Mu(sub0) ]

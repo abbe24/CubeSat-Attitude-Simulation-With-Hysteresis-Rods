b58568f75e0d171ac0ac0275df770fa9dc3a955b
OzMelzer <oz.melzer@gmail.com>
2025-11-01 11:50:07 -0600

    add in GASRATS Sattelite to simulation

--- Simulations/STKIntegration/BasicSTKIntegration1.m
+++ Simulations/STKIntegration/BasicSTKIntegration1.m
@@ -6,7 +6,7 @@ clear; clc; close all;
 
 %Launch STK application
 app = actxserver('STK12.application');
-root = app.Personality2;
+root = app.Personality2; %DONT KNOW WHAT THIS DOES
 %Make STK Application visible
 app.Visible = 1;
 
@@ -15,5 +15,8 @@ scenario = root.Children.New('eScenario','MATLAB_Integration');
 scenario.SetTimePeriod('1 Aug 2026 00:00:00.000','2 Aug 2026 00:00:00.000');
 scenario.StartTime = '1 Aug 2026 00:00:00.000';
 scenario.StopTime = '2 Aug 2026 00:00:00.000';
-root.ExecuteCommand('Animate * Reset');
+root.ExecuteCommand('Animate * Reset'); %DONT KNOW WHAT THIS DOES
+
+%create satellite
+satellite = scenario.Children.New('eSatellite','GASRATS');
 

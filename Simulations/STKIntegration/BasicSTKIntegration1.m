35f2a245030a71dfc5e384ce8acf1d08958b45c3
OzMelzer <oz.melzer@gmail.com>
2025-11-01 10:33:13 -0600

    created basic matlab integration with STK

---
+++ Simulations/STKIntegration/BasicSTKIntegration1.m
@@ -0,0 +1,19 @@
+
+clear; clc; close all;
+
+%The purpose of this code is to explore the possibility of integrating STK
+%with matlab.
+
+%Launch STK application
+app = actxserver('STK12.application');
+root = app.Personality2;
+%Make STK Application visible
+app.Visible = 1;
+
+%create Scenario from 08/01/2026 - 08/02/2026 named Matlab_Integration
+scenario = root.Children.New('eScenario','MATLAB_Integration');
+scenario.SetTimePeriod('1 Aug 2026 00:00:00.000','2 Aug 2026 00:00:00.000');
+scenario.StartTime = '1 Aug 2026 00:00:00.000';
+scenario.StopTime = '2 Aug 2026 00:00:00.000';
+root.ExecuteCommand('Animate * Reset');
+

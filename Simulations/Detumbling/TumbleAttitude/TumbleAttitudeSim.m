c7ba7e15651790c652471704778f0ec9e8427a83
OzMelzer <oz.melzer@gmail.com>
2025-11-08 11:04:27 -0700

    outlined attitude tubling sim with no torques, Inputs, Outpus, questions

--- Simulations/Detumbling/TumbleAttitude/TumbleAttitudeSim.m
+++ Simulations/Detumbling/TumbleAttitude/TumbleAttitudeSim.m
@@ -1,1 +1,18 @@
-%Display attitude of cubesat as it tumbles
+%{
+
+QUESTIONS: Euler angle rates vs. Angular Velocities?, Body Fixed Frame vs
+intertial Frame? 
+
+The purpose of this simulation is to simulate a tumbling 1.5U cubesat for 100
+orbits starting on AUG 1 2026 00:00:00 based on initial angular velocities using the Newton-Euler rotational 
+Equations of motion for rigid-bodies. 
+
+INPUTS: ISS Longitude Latitude Altitude ORBIT DATA (from STK), WMM Magnetic flux density data
+(From STK), Moments of inertia of cubesat (IXX, IYY, IZZ), Initial Angular
+Velocities (Wx, Wy, Wz), Initial Euler Angles (Phi, Theta, Psi).
+
+Outputs: Euler angles over 100 orbits (Phi, Theta, Psi), Euler angle rates
+over 100 orbits (PhiDot, ThetaDot, PsiDot), Angular Velocities in ECEF
+Frame over 100 orbits
+
+%}

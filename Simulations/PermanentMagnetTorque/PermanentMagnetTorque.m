e91e029ffee807e9fbd4dad83e84bb431aef77db
Camwdick <camwdick@gmail.com>
2025-11-15 00:37:01 -0700

    Started working on Magnet Torque

--- Simulations/PermanentMagnetTorque/PermanentMagnetTorque.m
+++ Simulations/PermanentMagnetTorque/PermanentMagnetTorque.m
@@ -1,1 +1,9 @@
-placeholder
+%{
+
+The purpose of this section is to calculate the torque generated via a
+Permanent Magnet based of eq. 1 in the PMAC paper.
+
+
+lol i did more stuff on stk than matlab
+%}
+

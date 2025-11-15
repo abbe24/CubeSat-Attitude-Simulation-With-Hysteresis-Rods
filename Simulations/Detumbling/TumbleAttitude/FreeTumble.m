d208b3255686f37ef52aaf4094760f5812c3d8a1
OzMelzer <oz.melzer@gmail.com>
2025-11-15 08:43:41 -0700

    create FreeTumble function for ODE45 to use

--- Simulations/Detumbling/TumbleAttitude/FreeTumble.m
+++ Simulations/Detumbling/TumbleAttitude/FreeTumble.m
@@ -1,1 +1,19 @@
-placeholder
+function dydt = FreeTumble(t, y, I)
+% Computes the derivative of angular velocity for free tumbling (no torques)
+% y = [wx; wy; wz]
+
+wx = y(1);
+wy = y(2);
+wz = y(3);
+
+Ixx = I(1);
+Iyy = I(2);
+Izz = I(3);
+
+% Euler rotational equations (no torques)
+wx_dot = ((Iyy - Izz)/Ixx) * wy * wz;
+wy_dot = ((Izz - Ixx)/Iyy) * wz * wx;
+wz_dot = ((Ixx - Iyy)/Izz) * wx * wy;
+
+dydt = [wx_dot; wy_dot; wz_dot];
+end
